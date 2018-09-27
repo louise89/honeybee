7561af64826f45887a40bb354e5da002285d5520
Louise Simpson <louise.simpson@onthebeach.co.uk>
2018-09-27 10:51:21 +0100

    Add Recipe controller.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,4 +9,47 @@ class RecipesController < ApplicationController
     @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions = {})
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :index
+    end
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.destroy
+      redirect_to recipes_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
+  end
 end
