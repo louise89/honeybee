2f594ec33d854161bb1eeec523cb8e1e800ec288
Louise Simpson <louise.simpson@onthebeach.co.uk>
2018-10-05 13:56:43 +0100

    update testing spec

--- spec/controllers/ingredients_controller_spec.rb
+++ spec/controllers/ingredients_controller_spec.rb
@@ -5,28 +5,28 @@ RSpec.describe IngredientsController, type: :controller do
   describe 'GET #index' do
     it "index action should render index template" do
       get :index
-      response.should render_template(:index)
+      expect(response).to render_template('index')
     end
   end
 
   describe 'GET#show' do
     it "show action should render show template" do
       get :show, id: Ingredient.first
-      response.should render_template(:show)
+      expect(response).to render_template('show')
     end
   end
 
   describe 'GET#new' do
     it "new action should render new template" do
       get :new
-      response.should render_template(:new)
+      expect(response).to render_template('new')
     end
   end
 
   describe 'GET #edit' do
     it "edit action should render edit template" do
-      get :edit, id:  Ingredient.first
-      response.should render_template(:edit)
+      get :edit, id: Ingredient.first
+      expect(response).to render_template('edit')
     end
   end
 
@@ -34,7 +34,7 @@ RSpec.describe IngredientsController, type: :controller do
     it "update action should render edit template when model is invalid" do
       Ingredient.any_instance.stubs(:valid?).returns(false)
       put :update, id: Ingredient.first
-      response.should render_template(:edit)
+      expect(response).to render_template('edit')
     end
 
     it "update action should redirect when model is valid" do
@@ -61,7 +61,7 @@ RSpec.describe IngredientsController, type: :controller do
   describe 'DELETE #destroy' do
   it "destroy action should destroy model and redirect to index action" do
       @ingredient = Ingredient.first
-      delete :destroy, id:  @ingredient
+      delete :destroy, id: @ingredient
       # response.should redirect_to(<%= items_path('url') %>)
       Ingredient.exists?(@ingredient.id).should be_false
     end
