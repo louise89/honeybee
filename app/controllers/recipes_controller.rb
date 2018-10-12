3422435b3653c9cd71c62d3a82e0af5bbd51eb48
Louise Simpson <louise.simpson@onthebeach.co.uk>
2018-10-12 15:11:56 +0100

    refactor ingredients.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -50,6 +50,6 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :description)
+    params.require(:recipe).permit(:name, :description, :user_id)
   end
 end
