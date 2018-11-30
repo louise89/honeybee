1b4b8be57e9c320f7ee1afc9e4810b3538882c80
Louise Simpson <louise.simpson@onthebeach.co.uk>
2018-11-30 14:50:17 +0000

    add testing to recipe ingredient spec

--- spec/models/recipe_ingredient_spec.rb
+++ spec/models/recipe_ingredient_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe RecipeIngredient, type: :model do
+  let(:recipe_ingredient) { RecipeIngredient.new(quantity: quantity, ingredient: ingredient, recipe: recipe) }
+  let(:ingredient) { Ingredient.new(name: ingredient_name) }
+  let(:recipe) { Recipe.new(name: recipe_name, description: description) }
+  let(:ingredient_name) { 'Carrot' }
+  let(:quantity) { 1 }
+  let(:recipe_name) { 'Chicken Pasta' }
+  let(:description) { 'recipe description' }
 
+  describe '#name' do
+    subject { recipe_ingredient.name }
+
+    before do
+      allow(recipe_ingredient).to receive(:ingredient).and_return(ingredient)
+    end
+
+    it { is_expected.to eq 'Carrot' }
+  end
+
+  describe '#quantity' do
+    subject { recipe_ingredient.increase_quantity(new_quantity) }
+
+    let(:new_quantity) { 2 }
+
+    it { is_expected.to be true }
+
+    it 'updates the quantity to include the new quantity' do
+      subject
+      expect(recipe_ingredient.quantity).to eq(3)
+    end
+  end
 end
