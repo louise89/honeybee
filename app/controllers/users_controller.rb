9a7e555c4cb1c6b4db27e4c8d11bd8b472b7eb8d
Michael Hibbs <michael.hibbs@onthebeach.co.uk>
2018-12-09 17:48:02 +0000

    Add User edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,42 @@
 class UsersController < ApplicationController
   before_action :user
+  before_action :ensure_can_edit, only: [ :edit, :update ]
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if user.update(user_params)
+      flash[:notice] = 'User updated successfully'
+      redirect_to user_path(user)
+    else
+      render :edit
+    end
   end
 
   private
 
   def user
     @user ||= User.find(params[:id])
+  end
+
+  def ensure_can_edit
+    if !can_edit?
+      redirect_to user_path(@user)
+    end
+  end
+
+  def can_edit?
+    current_user && current_user === @user
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+    )
   end
 end
