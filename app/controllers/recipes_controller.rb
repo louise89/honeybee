6895a790a1c4f0802107ad5e5b7babab1e064c1b
Michael Hibbs <michael.hibbs@onthebeach.co.uk>
2018-12-14 16:11:35 +0000

    Add create, edit and update methods to the recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,31 @@
 class RecipesController < ApplicationController
-
   def new
     @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      flash[:notice] = 'congratulations'
+      redirect_to edit_recipe_path(@recipe)
+    else
+      flash[:alert] = 'You dun fucked up'
+      render :new
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :user_id)
   end
 end
--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -2,10 +2,106 @@ require 'rails_helper'
 
 RSpec.describe RecipesController, type: :controller do
 
+  let(:user) { User.create!(name: 'test', email: email, password: password) }
+  let(:email) { 'test@test.com' }
+  let(:password) { 'louiseisverycool' }
+
   describe '#new' do
     it 'assigns a new recipe' do
       get :new
       expect(assigns[:recipe]).to be_a_new(Recipe)
+    end
+  end
+
+  describe '#create' do
+    let(:post_request) do
+      post :create, params: {
+        recipe: {
+          name: 'Choco bits',
+          description: 'Super Chocolatey',
+          user_id: user.id
+        }
+      }
+    end
+
+    it 'creates a new recipe' do
+      expect {
+        post_request
+      }.to change(Recipe, :count).by(1)
+    end
+
+    it 'shows the flash message' do
+      post_request
+
+      expect(flash[:notice]).to eql('congratulations')
+    end
+
+    it 'redirects to the edit page' do
+      expect(post_request).to redirect_to(edit_recipe_path(Recipe.last))
+    end
+
+    context 'when there are invalid attributes' do
+      let(:post_request) do
+        post :create, params: {
+          recipe: {
+            name: '',
+            description: '',
+            user_id: user.id
+          }
+        }
+      end
+
+      it 'does not create a new recipe' do
+        expect {
+          post_request
+        }.to_not change { Recipe.count }
+      end
+
+      it 'shows the flash message' do
+        post_request
+
+        expect(flash[:alert]).to eql('You dun fucked up')
+      end
+
+      it 'renders the new action' do
+        post_request
+
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe '#edit' do
+    let(:recipe) { Recipe.create!(name: name, description: description, user: user) }
+    let(:name) { 'existing recipe' }
+    let(:description) { 'existing recipe description' }
+
+    it 'assigns an existing recipe' do
+      get :edit, params: { id: recipe.id }
+
+      expect(assigns[:recipe]).to eql(recipe)
+    end
+  end
+
+  describe '#update' do
+    # let(:recipe) { Recipe.create!(name: name, description: description, user: user) }
+    # let(:name) { 'existing recipe' }
+    # let(:description) { 'existing recipe description' }
+    #
+    # let(:put_request) do
+    #   put :update, params: {
+    #     recipe: {
+    #       name: new_name,
+    #       description: new_description,
+    #       user_id: user.id
+    #     }
+    #   }
+    # end
+    # let(:new_name) { 'new name' }
+    # let(:new_description) { 'new desc' }
+
+    it 'updates the requested recipe' do
+      put_request
     end
   end
 end
