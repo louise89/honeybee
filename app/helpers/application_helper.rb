22c107e7a755141151d70667bc9d7340b2b5e5fc
Louise Simpson <louise.simpson@onthebeach.co.uk>
2019-01-16 13:18:01 +0000

    refactor into helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def can_edit_recipe?(recipe)
+    current_user.present? && (
+      recipe.owned_by?(current_user) || current_user.admin?
+    )
+  end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper do
+  let(:recipe) { Recipe.create!(name: 'Chicken', description: 'Chicken Steps', user: recipe_user) }
+
+  let(:recipe_user) { User.create!(email: 'test@test.com', name: 'Recipe User', password: '1234567') }
+
+  let(:current_user) { User.create!(email: 'test2@test.com', name: 'Other User', password: '1234567', admin: admin?) }
+  let(:admin?) { false }
+
+  before do
+    sign_in current_user if current_user
+  end
+
+  describe 'can_edit_recipe?' do
+    subject { helper.can_edit_recipe?(recipe) }
+
+    context 'when a user is the recipe owner' do
+      let(:current_user) { recipe_user }
+      it { is_expected.to be true }
+    end
+
+    context 'when a user is not a admin or an owner' do
+      let(:admin?) { false }
+      let(:current_user) { nil }
+
+      it { is_expected.to be false }
+    end
+
+    context 'when a user is an admin but not an owner' do
+      let(:admin?) { true }
+
+      it { is_expected.to be true }
+    end
+  end
+end
